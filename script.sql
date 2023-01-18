6537e8abcbbc54a6092613cd32a725e29f9478c9
espina88 <48732731+espina88@users.noreply.github.com>
2023-01-18 17:26:09 +0100

    Adding clause to database creation syntax

--- script.sql
+++ script.sql
@@ -1,5 +1,7 @@
+CREATE DATABASE IF NOT EXISTS ibm;
+use ibm;
 CREATE TABLE IF NOT EXISTS proveedores (id_proveedor int NOT NULL,nombre varchar(45),fecha_alta date,id_cliente int,PRIMARY KEY(id_proveedor));
 INSERT INTO proveedores (id_proveedor,nombre,fecha_alta,id_cliente) 
-VALUES(1,'Coca-cola','dd/mm/yyyy',5),
-(2,'Pepsi','dd/mm/yyyy',5),
-(3,'Redbull','dd/mm/yyyy',6);
+VALUES(1,'Coca-cola','2021-03-02',5),
+(2,'Pepsi','2022-01-02',5),
+(3,'Redbull','2020-03-02',6);
